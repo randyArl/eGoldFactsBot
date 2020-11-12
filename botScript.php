3ae9371187fa7a6fc35eede5560183c9efc5542d
Randy Aranta Lika <Randy_arantalika@yahoo.com>
2020-11-12 10:29:59 +0800

    Update botScript.php

--- botScript.php
+++ botScript.php
@@ -2,7 +2,7 @@
  
 $botToken = "1475059231:AAGW6Q8nM5dZFAZOM0mtxDhHHdEdVtlaLJc";
 $website = "https://api.telegram.org/bot".$botToken;
- 
+  
 $update = file_get_contents('php://input');
 $update = json_decode($update, TRUE);
  
@@ -14,25 +14,15 @@ $message = $update["message"]["text"];
 switch($message) {
     
     case "/about":
-        forwardMessage($chatId, 3);
+        sendMessage($chatId, "test");
         break;
     case "/egoldfacts":
-        forwardMessage($chatId, mt_rand(4,60));
+        sendMessage($chatId, "hi there!");
         break;
     default: 
-        sendMessage($chatId, "hello");
+        sendMessage($chatId, "default");
     
 }
-    
-}
- 
-function forwardMessage ($chatId, $message) {
-    
-    $url = $GLOBALS[website]."/forwardMessage?chat_id=".$chatId."&from_chat_id=@eGoldFacts"."&message_id=".$message);
-    file_get_contents($url);
-    
-}
- 
  
 function sendMessage ($chatId, $message) {
     
@@ -40,6 +30,9 @@ function sendMessage ($chatId, $message) {
     file_get_contents($url);
     
 }
+ 
+ 
+ 
  
  
 ?>
